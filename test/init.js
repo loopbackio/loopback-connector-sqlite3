7d672a2fa38d577553dafc66148bf1e63965dc19
Krishna Raman <kraman@gmail.com>
2015-07-09 15:21:18 -0700

    migration: Check table exists within transaction

--- test/init.js
+++ test/init.js
@@ -1,11 +1,19 @@
 var DataSource = require('loopback-datasource-juggler').DataSource;
+var path = require('path');
+var os = require('os');
 
-var config = require('rc')('loopback', {test: {sqlite: {}}}).test.sqlite;
+var config = require('rc')('loopback', {
+  test: {
+    sqlite: {
+      file: path.join(os.tmpdir(), 'test-' + Date.now().toString() + '.db')
+    }
+  }
+}).test.sqlite;
 
-global.getDataSource = global.getSchema = function () {
+global.getDataSource = global.getSchema = function() {
   var db = new DataSource(require('../'), config);
-  db.log = function (a) {
-     console.log(a);
+  db.log = function(a) {
+    console.log(a);
   };
   return db;
 };
--- test/sqlite.test.js
+++ test/sqlite.test.js
@@ -4,12 +4,13 @@ require('./init');
 
 var should = require('should');
 
-var Post, db;
+var Post;
+var db;
 
 /*global describe, before, it, getDataSource*/
-describe('sqlite3 connector', function () {
+describe('sqlite3 connector', function() {
 
-  before(function () {
+  before(function() {
     db = getDataSource();
 
     Post = db.define('PostWithBoolean', {
@@ -20,8 +21,8 @@ describe('sqlite3 connector', function () {
     });
   });
 
-  it('should run migration', function (done) {
-    db.automigrate('PostWithBoolean', function () {
+  it('should run migration', function(done) {
+    db.automigrate('PostWithBoolean', function() {
       done();
     });
   });
@@ -154,17 +155,17 @@ describe('sqlite3 connector', function () {
  });
 
  test.it('should not generate malformed SQL for number columns set to empty ' +
-  'string', function (test) {
+  'string', function(test) {
  var Post = dataSource.define('posts', {
  title: { type: String }
  , userId: { type: Number }
  });
  var post = new Post({title:'no userId', userId:''});
 
- Post.destroyAll(function () {
- post.save(function (err, post) {
+ Post.destroyAll(function() {
+ post.save(function(err, post) {
  var id = post.id
- Post.all({where:{title:'no userId'}}, function (err, post) {
+ Post.all({where:{title:'no userId'}}, function(err, post) {
  test.ok(!err);
  test.ok(post[0].id == id);
  test.done();
@@ -173,13 +174,13 @@ describe('sqlite3 connector', function () {
  });
  });
 
- test.it('all should support regex', function (test) {
+ test.it('all should support regex', function(test) {
  Post = dataSource.models.Post;
 
- Post.destroyAll(function () {
- Post.create({title:'PostgreSQL Test Title'}, function (err, post) {
+ Post.destroyAll(function() {
+ Post.create({title:'PostgreSQL Test Title'}, function(err, post) {
  var id = post.id
- Post.all({where:{title:/^PostgreSQL/}}, function (err, post) {
+ Post.all({where:{title:/^PostgreSQL/}}, function(err, post) {
  test.ok(!err);
  test.ok(post[0].id == id);
  test.done();
@@ -188,11 +189,11 @@ describe('sqlite3 connector', function () {
  });
  });
 
- test.it('all should support arbitrary expressions', function (test) {
- Post.destroyAll(function () {
- Post.create({title:'PostgreSQL Test Title'}, function (err, post) {
+ test.it('all should support arbitrary expressions', function(test) {
+ Post.destroyAll(function() {
+ Post.create({title:'PostgreSQL Test Title'}, function(err, post) {
  var id = post.id
- Post.all({where:{title:{ilike:'postgres%'}}}, function (err, post) {
+ Post.all({where:{title:{ilike:'postgres%'}}}, function(err, post) {
  test.ok(!err);
  test.ok(post[0].id == id);
  test.done();
@@ -201,12 +202,12 @@ describe('sqlite3 connector', function () {
  });
  })
 
- test.it('all should support like operator ', function (test) {
+ test.it('all should support like operator ', function(test) {
  Post = dataSource.models.Post;
- Post.destroyAll(function () {
- Post.create({title:'PostgreSQL Test Title'}, function (err, post) {
+ Post.destroyAll(function() {
+ Post.create({title:'PostgreSQL Test Title'}, function(err, post) {
  var id = post.id
- Post.all({where:{title:{like:'%Test%'}}}, function (err, post) {
+ Post.all({where:{title:{like:'%Test%'}}}, function(err, post) {
  test.ok(!err);
  test.ok(post[0].id == id);
  test.done();
@@ -215,12 +216,12 @@ describe('sqlite3 connector', function () {
  });
  });
 
- test.it('all should support \'not like\' operator ', function (test) {
+ test.it('all should support \'not like\' operator ', function(test) {
  Post = dataSource.models.Post;
- Post.destroyAll(function () {
- Post.create({title:'PostgreSQL Test Title'}, function (err, post) {
+ Post.destroyAll(function() {
+ Post.create({title:'PostgreSQL Test Title'}, function(err, post) {
  var id = post.id
- Post.all({where:{title:{nlike:'%Test%'}}}, function (err, post) {
+ Post.all({where:{title:{nlike:'%Test%'}}}, function(err, post) {
  test.ok(!err);
  test.ok(post.length===0);
  test.done();
@@ -229,12 +230,12 @@ describe('sqlite3 connector', function () {
  });
  });
 
- test.it('all should support arbitrary where clauses', function (test) {
+ test.it('all should support arbitrary where clauses', function(test) {
  Post = dataSource.models.Post;
- Post.destroyAll(function () {
- Post.create({title:'PostgreSQL Test Title'}, function (err, post) {
+ Post.destroyAll(function() {
+ Post.create({title:'PostgreSQL Test Title'}, function(err, post) {
  var id = post.id;
- Post.all({where:"title = 'PostgreSQL Test Title'"}, function (err, post) {
+ Post.all({where:"title = 'PostgreSQL Test Title'"}, function(err, post) {
  test.ok(!err);
  test.ok(post[0].id == id);
  test.done();
@@ -244,12 +245,12 @@ describe('sqlite3 connector', function () {
  });
 
  test.it('all should support arbitrary parameterized where clauses',
- function (test) {
+ function(test) {
  Post = dataSource.models.Post;
- Post.destroyAll(function () {
- Post.create({title:'PostgreSQL Test Title'}, function (err, post) {
+ Post.destroyAll(function() {
+ Post.create({title:'PostgreSQL Test Title'}, function(err, post) {
  var id = post.id;
- Post.all({where:['title = ?', 'PostgreSQL Test Title']}, function (err, post) {
+ Post.all({where:['title = ?', 'PostgreSQL Test Title']}, function(err, post) {
  test.ok(!err);
  test.ok(post[0].id == id);
  test.done();
